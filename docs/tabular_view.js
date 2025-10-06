b9c82612bd417ddebfb90dd6e82627e1ce7d6e8c
theophinetheodore <lesgooo1993@proton.me>
2025-10-07 02:06:09 +0530

    2025 oct 07

--- docs/tabular_view.js
+++ docs/tabular_view.js
@@ -4,13 +4,24 @@ const textArray = text.innerHTML.split("\n");
 let html_str = "";
 
 for (var i=0; i<textArray.length; i++) {
-  if (textArray[i].startsWith("~ Start")) {
+  if (textArray[i].startsWith("~")) {
     html_str += `<tr><td colspan='2'>${textArray[i]}</td></tr>`
+  } else if
+    (textArray[i].startsWith("M-") ||
+    textArray[i].startsWith("F-") ||
+    textArray[i].startsWith("C-")) {
+      console.log("haha");
+
+      const blocks = textArray[i].split("-");
+
+      html_str += `<tr><td>${blocks[0]}</td>`;
+      html_str += `<td>${blocks[1]}</td></tr>`;
   } else if
     (textArray[i].startsWith("M+F") ||
     textArray[i].startsWith("M") ||
     textArray[i].startsWith("C") ||
     textArray[i].startsWith("F")) {
+      console.log("bruh")
       html_str += `<tr><td>${textArray[i]}</td>`
       html_str += `<td>${textArray[i + 1]}`
       html_str += `${textArray[i + 2]}</td></tr>`

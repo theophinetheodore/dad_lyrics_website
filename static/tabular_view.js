38eb4476683e5bd8368eebf86f33b7a3f78408af
theophinetheodore <lesgooo1993@proton.me>
2025-10-24 00:22:25 +0530

    fixed script execution error (or just say hack?)

--- static/tabular_view.js
+++ static/tabular_view.js
@@ -1,36 +1,38 @@
-const text = document.getElementById('single-content').firstElementChild;
-const textArray = text.innerHTML.split("\n");
+document.addEventListener("DOMContentLoaded", (e) => {
+  const text = document.getElementById('single-content').firstElementChild;
+  const textArray = text.innerHTML.split("<br>");
 
-let html_str = "";
+  let html_str = "";
 
-for (var i=0; i<textArray.length; i++) {
-  if (textArray[i].startsWith("~")) {
+  for (var i=0; i<textArray.length; i++) {
+    if (textArray[i].startsWith("~")) {
 
-    html_str += `<div><b>${textArray[i]}</b></div>`
-    html_str += `<hr id="notation-divider">`
+      html_str += `<div><b>${textArray[i]}</b></div>`
+      html_str += `<hr id="notation-divider">`
 
-  } else if (textArray[i].startsWith("M") || textArray[i].startsWith("C") || textArray[i].startsWith("F")) {
+    } else if (textArray[i].startsWith("M") || textArray[i].startsWith("C") || textArray[i].startsWith("F")) {
 
-    html_str += `<div><b>${textArray[i]}</b></div>`
+      html_str += `<div><b>${textArray[i]}</b></div>`
 
-  } else if (textArray[i].startsWith(".")) {
+    } else if (textArray[i].startsWith(".")) {
 
-    html_str += `<hr id="notation-divider">`
+      html_str += `<hr id="notation-divider">`
 
-  } else if (textArray[i].includes("Link")) {
+    } else if (textArray[i].includes("Link")) {
 
-    html_str += `<div style="text-align: right;"><b>${textArray[i]}</b></div>`
+      html_str += `<div style="text-align: right;"><b>${textArray[i]}</b></div>`
 
-  } else if (textArray[i].startsWith("(")) {
+    } else if (textArray[i].startsWith("(")) {
 
-    html_str += `<div style="text-align: right;"><b>${textArray[i]}</b></div>`
-    html_str += `<hr id="notation-divider">`
+      html_str += `<div style="text-align: right;"><b>${textArray[i]}</b></div>`
+      html_str += `<hr id="notation-divider">`
 
-  } else {
+    } else {
 
-    html_str += `<div style="text-align: center;">${textArray[i]}</div>`
+      html_str += `<div style="text-align: center;">${textArray[i]}</div>`
 
+    }
   }
-}
 
-document.getElementById('notation-container').innerHTML = html_str;
+  document.getElementById('notation-container').innerHTML = html_str;
+})
